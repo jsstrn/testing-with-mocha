8c019a2c852bd0d115deb651108a02678dd1dd97
Jesstern Rays <jsstrn@Jessterns-MacBook-Air.local>
2016-04-08 15:47:27 +0800

    Delete test files

--- test/multiply-test-chai-expect.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect
-const multiply = require('../multiply')
-
-describe('multiply with expect', () => {
-  it('should return correct result of 5 x 10', () => {
-    expect(multiply(5, 10)).to.equal(5 * 10)
-  })
-})
