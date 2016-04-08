9212c2568b6fb0576b0fd6149f166c779aa8f558
Jesstern Rays <jsstrn@Jessterns-MacBook-Air.local>
2016-04-08 16:00:10 +0800

    Replace ES6 arrow functions with ES5 functions for compatibility with Mocha

--- test/multiply-test.js
+++ test/multiply-test.js
@@ -1,24 +1,23 @@
 /* global describe, it */
-'use strict'
 const assert = require('assert')
 const expect = require('chai').expect
 const should = require('chai').should()
 const multiply = require('../multiply')
 
-describe('multiply with assert', () => {
-  it('should return correct result of 5 x 10', () => {
+describe('multiply with assert', function () {
+  it('should return correct result of 5 x 10', function () {
     assert.equal(multiply(5, 10), (5 * 10))
   })
 })
 
-describe('multiply with expect', () => {
-  it('should return correct result of 5 x 10', () => {
+describe('multiply with expect', function () {
+  it('should return correct result of 5 x 10', function () {
     expect(multiply(5, 10)).to.equal(5 * 10)
   })
 })
 
-describe('multiply with should', () => {
-  it('should return correct result of 5 x 10', () => {
+describe('multiply with should', function () {
+  it('should return correct result of 5 x 10', function () {
     multiply(5, 10).should.equal(5 * 10)
   })
 })
